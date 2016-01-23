d33fdd0106d5676d197d86f7834f284c662abc0e
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-01-23 20:57:19 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 
-local fuse = '#DearAdminًںکœ we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusernameâ–¶ï¸ڈ :@ ' .. msg.from.username  ..'\n\pmn :\n\n\n' .. matches[1] 
+local fuse = ' DearAdminًںکœ we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusernameâ–¶ï¸ڈ :@ ' .. msg.from.username  ..'\n\pmn :\n\n\n' .. matches[1] 
 local fuses = '!printf user#id' .. msg.from.id
 
 
