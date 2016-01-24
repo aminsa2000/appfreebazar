f16f34a7ba57739ace7f6e22e1755aa9862a90a6
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-01-24 13:56:15 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -1,28 +1,43 @@
 do
- function run(msg, matches)
- 
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
- local fuses = '!printf user#id' .. msg.from.id
- 
- 
-   local text = matches[1]
-   local chat = "chat#id"..70690378 
-   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Sent!'
- 
- end
- end
- return {
-  
-  description = "Feedback",
- 
-  usage = "!feedback message",
+
+function run(msg, matches)
+
+local fuse = ' #DearAdmin we have recived a new feedback just now : #newfeedback \n\n id  : ' .. msg.from.id .. '\n\nNAME : ' .. msg.from.print_name ..'\n\nusername : @ ' .. msg.from.username  ..'\pm :\n\n' .. matches[1] 
+local fuses = '!printf user#id' .. msg.from.id
+
+
+    local text = matches[1]
+ bannedidone = string.find(msg.from.id, '123')
+        bannedidtwo =string.find(msg.from.id, '465')       
+   bannedidthree =string.find(msg.from.id, '678')  
+
+
+        print(msg.to.id)
+
+        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
+                return 'You are banned to send a feedback'
+ else
+
+
+                 local sends0 = send_msg('chat#70690378', fuse, ok_cb, false)
+
+ return 'Your request has been sended to @Creed_is_dead and team 😜!'
+
+     
+
+end
+
+end
+return {
+  description = "Feedback to sudos",
+
+  usage = "!feedback : send maseage to admins with bot",
   patterns = {
-  "^[!/][Ff]eedback (.*)$"
- 
+    "^[!/]([Ff]eedback) (.*)$"
+    "^[Ff](eedback) (.*)$"
+
   },
   run = run
- }
-     --https://github.com/CreedsTeam
-    --Telegram.me/creed_is_dead
+}
+
+end
