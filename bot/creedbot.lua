106a5ab5e6c8278484540234ba452d7429eea739
m13790115 <golmahdi0@gmail.com>
2016-01-26 15:31:59 +0330

    Update creedbot.lua

--- bot/creedbot.lua
+++ bot/creedbot.lua
@@ -209,7 +209,7 @@ function create_config( )
     "ingroup",
     "inpm",
     "banhammer",
-    "Boobs",
+    ".",
     "Feedback",
     "plugins",
     "lock_join",
@@ -239,7 +239,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {139534999},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -250,10 +250,7 @@ function create_config( )
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
-   〰〰〰〰〰〰〰〰
+   ߔࠀ   @rm13790115   〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
    !feedback (your ideas and messages)
